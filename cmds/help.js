e2911892fd5ab9474009b4df4ccbc071256d679b
jahdhjajsixuxhh <ponfreemail@gmail.com>
2024-05-23 22:26:55 +0800

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -4,16 +4,17 @@ module.exports = {
     credits: "rejardgwapo",
     cooldown: 16,		
     execute(api, event, args, commands) {
-        let helpMessage = '𝙷𝚒! 𝚃𝚑𝚒𝚜 𝙱𝚘𝚝 𝚒𝚜 𝚌𝚛𝚎𝚊𝚝𝚎𝚍 𝚊𝚝 𝚢𝚎𝚝𝚊𝚗𝚘𝚝𝚑𝚎𝚛𝚏𝚋𝚋𝚘𝚝.𝚣𝚢𝚊.𝚖𝚎. 𝙷𝚎𝚛𝚎 𝚊𝚛𝚎 𝚊𝚕𝚕 𝚝𝚑𝚎 𝚌𝚘𝚖𝚖𝚊𝚗𝚍𝚜 𝚒𝚗𝚜𝚝𝚊𝚕𝚕𝚎𝚍 𝚘𝚗 𝚝𝚑𝚒𝚜 𝚜𝚎𝚛𝚟𝚎𝚛\n';
-        helpMessage += '💮═══════════════💮\n';
+        let helpMessage = '𝙷𝚒! 𝙷𝚎𝚛𝚎 𝚊𝚛𝚎 𝚊𝚕𝚕 𝚝𝚑𝚎 𝚌𝚘𝚖𝚖𝚊𝚗𝚍𝚜 𝚒𝚗𝚜𝚝𝚊𝚕𝚕𝚎𝚍 𝚘𝚗 𝚝𝚑𝚒𝚜 𝚜𝚎𝚛𝚟𝚎𝚛\n';
+        helpMessage += '❄️┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉❄️\n';
         commands.forEach((command, name) => {
-            helpMessage += `𝙽𝚊𝚖𝚎: ${name}\n`;
-            helpMessage += `𝙳𝚎𝚜𝚌𝚛𝚒𝚙𝚝𝚒𝚘𝚗: ${command.description}\n`;
-            helpMessage += `𝚁𝚘𝚕𝚎: ${command.role}\n`;
-            helpMessage += `Credits: ${command.credits}\n`;
-        helpMessage += '💮═══════════════💮\n';
+            helpMessage += `✓ 𝙽𝚊𝚖𝚎: ${name}\n`;
+            helpMessage += `✓ 𝙳𝚎𝚜𝚌𝚛𝚒𝚙𝚝𝚒𝚘𝚗: ${command.description}\n`;
+            helpMessage += `✓ 𝚁𝚘𝚕𝚎: ${command.role}\n`;
+            helpMessage += `✓ Credits: ${command.credits}\n`;
+        helpMessage += '❄️┉┉┉┉┉┉┉┉┉┉┉┉┉┉┉❄️\n';
         });
-        helpMessage += '💬https://facebook.com/yetanotherfbbot';
+        helpMessage +='';
+        helpMessage += '❄️ https://www.facebook.com/profile.php?id=61558861324061';
         api.sendMessage(helpMessage, event.threadID);
     }
 };
